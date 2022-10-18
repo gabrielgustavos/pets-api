1ac0d35df1604e947902753d05f81ed60e540f9e
gabrielgustavos <gabriel_gustavosilva@hotmail.com>
2022-10-18 17:40:39 -0300

    new pages and components increase and a theme for buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 /* eslint-disable import/no-unresolved */
 import { render } from "react-dom";
+import { StrictMode, useState } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import SearchParams from "./components/SearchParams";
-import { StrictMode } from "react";
+import Details from "./pages/Details";
+import ThemeContext from "./context/ThemeContext";
 
 const App = () => {
+  const theme = useState("darkblue");
   return (
     <StrictMode>
-      <div>
-        <h1>Adopt me!</h1>
-        <SearchParams />
-      </div>
+      <ThemeContext.Provider value={theme}>
+        <BrowserRouter>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeContext.Provider>
     </StrictMode>
   );
 };
+
 render(<App />, document.getElementById("root"));
